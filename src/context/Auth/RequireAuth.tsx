27c6ec7c89b185fcdebc61b655ebdd145a3cefcf
jack <juliocesar_19@hotmail.com.br>
2023-08-26 01:12:03 -0300

    sem criatividade

--- src/context/Auth/RequireAuth.tsx
+++ src/context/Auth/RequireAuth.tsx
@@ -2,22 +2,21 @@ import { ReactNode, useContext } from "react"
 import { AuthContext } from "./AuthContext"
 import Cookies from 'js-cookie';
 import { useApi } from "../../hooks/useApi";
+import { useNavigate } from "react-router-dom";
+import Login from "../../pages/Login";
 
 const api = useApi()
-
 interface RequireAuthProps {
     children: ReactNode;
   }
-
+  
 export const RequireAuth =  ({children}:{children: JSX.Element})=>{
+  const navigate = useNavigate();
     const auth = useContext(AuthContext);
-
-    const data = api.validadeToken() 
-    if(!data){
-    window.location.href='/login'
+    if(!auth.user){
+       return <Login/>
     }
-    return <>
-    {children}
-    </>
+    return children
+    
     
 }
